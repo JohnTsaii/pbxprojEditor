7aab408224b4b7ff484954af7b95bfb31dd320f9
caijingzhuang <caijingzhuang@gmail.com>
2017-10-13 16:27:14 +0800

    add commad line support

--- Project.pm
+++ Project.pm
@@ -11,6 +11,52 @@ use JSON -convert_blessed_universally;
 use Data::Structure::Util qw(unbless);
 use open ':encoding(utf8)';
 
+# subroutine declaration
+sub print_help();
+
+# OP
+if (scalar @ARGV) {
+  my $project = Project->new();
+
+  my @parameters; # use to push parameter that use to set_buildSettings_with_keyValue
+  my @args = map { $_ } @ARGV;
+  my $args = \@args;
+  my $index = 0;
+    foreach (@ARGV) {
+      $index ++;
+      if ($_ =~ /^-target/) {
+	my $target = $args[$index];
+	push @parameters, $target;
+	next;
+      }
+    
+      if ($_ =~ /^-configuration/) {
+	my $config = $args[$index];
+	push @parameters, $config;
+	next;
+      }
+    
+      if ($_=~ /=/) {
+	push @parameters, $_;
+      }
+    }
+  print @parameters;
+  exit;
+  $project->set_buildSettings_with_keyValue(@parameters);
+} else {
+  print_help();
+  exit;
+}
+
+# help desc
+sub print_help() {
+  print <<EOF
+-target          :specify the need be edited target.
+-configuration :specify the need be edited configuration, that must need be specify target
+EOF
+}
+
+# OO
 my $dst_dir;
 
 sub new() {
