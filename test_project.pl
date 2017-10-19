987595056ecb0cd18e038ea9f9d7d00edfbfe846
caijingzhuang <caijingzhuang@gmail.com>
2017-10-19 16:01:17 +0800

    change target

--- test_project.pl
+++ test_project.pl
@@ -7,8 +7,8 @@ use Xcode::Project;
 my $project = Project->new();
 
 sub test_PBXProject() {
-  $project->set_buildSettings_with_keyValue('RoadHome', 'Debug', "CODE_SIGN_IDENTITY=code1", "DEVELOPMENT_TEAM=12");
-  $project->set_buildSettings_with_keyValue('RoadHome', 'Release', "CODE_SIGN_IDENTITY=code1", "DEVELOPMENT_TEAM=12");
+  $project->set_buildSettings_with_keyValue('Target', 'Debug', "CODE_SIGN_IDENTITY=code1", "DEVELOPMENT_TEAM=12");
+  $project->set_buildSettings_with_keyValue('Target', 'Release', "CODE_SIGN_IDENTITY=code1", "DEVELOPMENT_TEAM=12");
   $project->save();
 }
 test_PBXProject();
